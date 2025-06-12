7ed118415ef55401c558da1725220da9198334ef
hanyuchen2012 <hanyuchen2012@hotmail.com>
2025-06-12 20:04:11 +0800

    common.rs

--- src/common.rs
+++ src/common.rs
@@ -996,7 +996,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "http://123.56.201.79:21114".to_owned()
 }
 
 #[inline]
